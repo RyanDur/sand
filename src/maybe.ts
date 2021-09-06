b8e8b5b146f43ccab433282d1b5deebdaa9d6efe
Ryan Durling <ryan.p.durling@gmail.com>
2021-09-06 14:31:20 -0500

    allow null into maybe

--- src/maybe.ts
+++ src/maybe.ts
@@ -24,6 +24,6 @@ const isNoneType = <T>(value: T): boolean => {
     return type === 'undefined' || type === 'null' || type === 'nan';
 };
 
-const of = <T>(value?: T, isNone = isNoneType): Maybe<T> => isNone(value) ? none() : some(value as T);
+const of = <T>(value?: T | null, isNone = isNoneType): Maybe<T> => isNone(value) ? none() : some(value as T);
 
 export const maybe = {of, none, some};
