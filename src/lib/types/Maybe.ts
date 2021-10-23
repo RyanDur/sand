0ec897ea136e8408c701c1895c5085909171d2de
Ryan Durling <ryan.p.durling@gmail.com>
2021-10-23 17:06:36 -0500

    update maybe interface

--- src/lib/types/Maybe.ts
+++ src/lib/types/Maybe.ts
@@ -1,10 +1,8 @@
-import {Supplier} from './Function';
-
 export interface Maybe<THING> {
     readonly isNothing: boolean;
     readonly orElse: (fallback: THING) => THING;
-    readonly orNull: Supplier<THING | null>;
+    readonly orNull: () => THING | null;
     readonly map: <NEW_THING>(f: (value: THING) => NEW_THING) => Maybe<NEW_THING>;
     readonly flatMap: <NEW_THING>(f: (value: THING) => Maybe<NEW_THING>) => Maybe<NEW_THING>;
-    readonly inspect: Supplier<string>;
+    readonly inspect: () => string;
 }
