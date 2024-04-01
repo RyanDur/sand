611f23b1102d39ac82a98b3378864f70578f3b26
Ryan Durling <ryan.p.durling@gmail.com>
2024-03-31 19:54:04 -0500

    export more utils

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ export {success, failure} from './lib/result';
 export {asyncResult, asyncSuccess, asyncFailure} from './lib/asyncResult';
 export {maybe, some, nothing} from './lib/maybe';
 export {
-    not,
-    empty,
-    has,
-    matchOn
+  not,
+  is,
+  empty,
+  notEmpty,
+  has,
+  matchOn
 } from './lib/util';
 
 export * from './lib/types';
